8ebe0ad3934e1f54717dd1fcd79a50344a2f9b07
Yan Alexandre <rm77740@fiap.com.br>
2021-08-18 16:29:31 -0300

    Adicionado download de PDF

--- src/app/servicos/lancamento.service.ts
+++ src/app/servicos/lancamento.service.ts
@@ -5,6 +5,8 @@ import { HttpService } from './http.service';
 import { environment as env } from 'src/environments/environment';
 import { Lancamento } from '../models/lancamento.model';
 import { saveAs } from 'file-saver';
+import jsPDF from 'jspdf';
+import 'jspdf-autotable';
 
 @Injectable({
   providedIn: 'root'
@@ -40,5 +42,35 @@ export class LancamentoService {
     saveAs(blob, 'lancamentos.csv');
   }
 
+  downloadPDF(lancamentos: Lancamento[]) {
+    const colunas = ['ID', 'Data', 'Hora', 'Tipo', 'Localização'];
+    const linhas: string[][] = [];
+    lancamentos.forEach(lanc => {
+      const dataHora = lanc.data.split(' ');
+      linhas.push([
+        lanc.id.toString(),
+        dataHora[0],
+        dataHora[1],
+        lanc.tipo,
+        lanc.localizacao
+      ]);
+    });
+    const data = new Date();
+    const doc = new jsPDF();
+    //const doc = new jsPDF('l', 'mm', 'a1');
+    (doc as any).text(15,10, 'Listagem de lançamentos');
+    (doc as any).text(170,10, data.toLocaleString('pt-BR'));
+    (doc as any).setFontSize(8);
+    (doc as any).autoTable({
+      head: [colunas],
+      body: linhas,
+      foot: [colunas],
+      theme: 'grid'
+    });
+    doc.save('lancamentos-' + data.toLocaleDateString('pt-BR') + '.pdf');
+    /*** Para solicitar ao usuario um nome para o pdf ***/
+    //const nome = window.prompt('digite um nome para o pdf: ');
+    //doc.save(nome?.toString());
 
+  }
 }
